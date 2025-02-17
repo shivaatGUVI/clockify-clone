914216e1fe9ce86ac41c65b5718bafb636d2fc74
shivaatGUVI <142791988+shivaatGUVI@users.noreply.github.com>
2025-02-17 12:16:39 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { AuthProvider } from "./contexts/LoginContext.jsx";
+import { AuthProvider } from "./context/LoginContext.jsx";
 import CategoryProvider from "./context/category/provider.jsx";
 import TrackerProvider from "./context/tracker/provider.jsx";
 
